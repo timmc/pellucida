39ae9438c59d3afc133c957234cbe65016e7b118
Tim McCormack <cortex@brainonfire.net>
2013-02-23 23:52:43 -0500

    Upgrade dep versions.

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
   :min-lein-version "2"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.3"]
-                 [hiccup "1.0.0"]
-                 [org.xerial/sqlite-jdbc "3.6.16"]
-                 [org.clojure/java.jdbc "0.1.1"]
-                 [enlive "1.0.1"]]
+                 [hiccup "1.0.2"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [enlive "1.1.1"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler org.timmc.pellucida/app
          :init org.timmc.pellucida/server-init!
--- src/org/timmc/pellucida/db.clj
+++ src/org/timmc/pellucida/db.clj
@@ -1,8 +1,7 @@
 (ns org.timmc.pellucida.db
   "Database helpers."
   (:require [org.timmc.pellucida.settings :as settings]
-            [clojure.java.jdbc :as sql]
-            clojure.java.jdbc.internal))
+            [clojure.java.jdbc :as sql]))
 
 (def ^:dynamic *db-spec*
   {:classname "org.sqlite.JDBC"
@@ -12,7 +11,7 @@
 
 (defmacro read-db ;; TODO make connection read-only
   [& body]
-  `(binding [clojure.java.jdbc.internal/*as-key* str]
+  `(binding [sql/*as-key* str]
      (sql/with-connection
        (assoc *db-spec* :subname (:gallery-db @settings/config))
        ~@body)))
