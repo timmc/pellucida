70ac019f34f7ca5b17c19c1bcdf011c96c82f4ec
Tim McCormack <cortex@brainonfire.net>
2013-05-05 00:11:38 -0400

    Periodically check if DB schema is useable.

--- src/org/timmc/pellucida/db.clj
+++ src/org/timmc/pellucida/db.clj
@@ -4,17 +4,54 @@
   (:require [org.timmc.pellucida.settings :as settings]
             [clojure.java.jdbc :as sql]))
 
+;;;; Connection spec
+
 (defonce ^:dynamic *db-spec*
   {:classname "org.sqlite.JDBC"
    :subprotocol "sqlite"
    ;; :subname "FILL IN"
    })
 
+;;;; Version checking
+
+(def acceptable-gallery-versions
+  #{0})
+
+(def last-check
+  "Results of last check."
+  (atom {:ts 0 :valid true :version nil}))
+
+(def min-check-interval
+  "Minimum version check interval, in millis."
+  (* 1 60 1000))
+
+(defn check-version
+  "Check version if we haven't checked recently."
+  []
+  (let [{:keys [ts valid version]} @last-check
+        now (System/currentTimeMillis)]
+    ;; Last check could be in future if clock has been changed. That's cool.
+    (if (< min-check-interval (Math/abs (- ts now)))
+      (let [cur-ver (sql/with-query-results r
+                      ["SELECT version FROM metadata"]
+                      (:version (first r)))]
+        (if (contains? acceptable-gallery-versions cur-ver)
+          (reset! last-check {:ts now :valid true :version cur-ver})
+          (do (reset! last-check {:ts now :valid false :version cur-ver})
+              (throw (RuntimeException.
+                      (str "Unexpected DB version, halting: " cur-ver))))))
+      (when-not valid
+        (throw (RuntimeException.
+                (str "Unexpected DB version on last check: " version)))))))
+
+;;;; --
+
 (defmacro read ;; TODO make connection read-only
   [& body]
   `(binding [sql/*as-key* str]
      (sql/with-connection
        (assoc *db-spec* :subname (:gallery-db settings/config))
+       (check-version)
        ~@body)))
 
 (defn jdbc-psql
