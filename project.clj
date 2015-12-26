5e2963a042950c01a4e413bec606b341fae8375d
Tim McCormack <cortex@brainonfire.net>
2015-12-25 19:37:59 -0500

    upgrade clojure, enlive

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main ^:skip-aot org.timmc.pellucida
   :min-lein-version "2"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.timmc/handy "1.5.0"]
                  [compojure "1.1.3"]
                  [ring/ring-jetty-adapter "1.1.8"]
                  [ring-reload-modified "0.1.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [org.clojure/java.jdbc "0.2.3"]
-                 [enlive "1.1.1"]]
+                 [enlive "1.1.6"]]
   :plugins [[org.timmc/lein-otf "2.0.0"]])
