44c20379c840b11b6a3b4869207ce5349e3d9a8c
Tim McCormack <cortex@brainonfire.net>
2016-01-05 15:11:50 -0500

    Use v2 image URLs in S3

--- conf/production.clj
+++ conf/production.clj
@@ -1,5 +1,5 @@
 {:thumbs-proxy-base nil
- :thumbs-link-base "https://s3.amazonaws.com/cdn-pellucida.gallery.brainonfire.net/kpawebgen/images/"
+ :thumbs-link-base "https://s3.amazonaws.com/cdn-pellucida.gallery.brainonfire.net/kpawebgen/images-v2/"
  :gallery-db "db/gallery.db3"
  :gmaps-api-key "ABQIAAAALcUXcqrPYFafhBFIoKYs_xTytYfssW4bhHQ4ZYqosPwIpnrsDBT26RDPsg9UE-RrAmtKEsKltys7_w"
  :btc-donate-addr "15rmT5e3xDxGScJspJSZ9RemKHhxuBmejN"}
