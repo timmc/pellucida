bc6e5ef151cca9203ca550f168a6e54898065fc7
Tim McCormack <cortex@brainonfire.net>
2016-04-26 23:41:44 -0400

    Remove trailing slash on base URL

--- conf/production.clj
+++ conf/production.clj
@@ -1,4 +1,4 @@
-{:base-url "https://pellucida.herokuapp.com/"
+{:base-url "https://pellucida.herokuapp.com"
  :thumbs-link-base "https://s3.amazonaws.com/cdn-pellucida.gallery.brainonfire.net/kpawebgen/images-v2/"
  :gallery-db "db/gallery.db3"
  :google-static-maps-v2-api-key-browser "AIzaSyALz6yR2Lw1liTI-97_5bwwGHBsZQH2SiM"
