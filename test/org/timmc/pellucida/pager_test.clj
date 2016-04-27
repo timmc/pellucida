384ea0960f891e9b73dda1bec2d5e66c70f06b1d
Tim McCormack <cortex@brainonfire.net>
2016-04-26 22:12:24 -0400

    Use qualified reference to ns-under-test

--- test/org/timmc/pellucida/pager_test.clj
+++ test/org/timmc/pellucida/pager_test.clj
@@ -1,10 +1,11 @@
 (ns org.timmc.pellucida.pager-test
-  (:use clojure.test
-        org.timmc.pellucida.pager))
+  (:use clojure.test)
+  (:require [org.timmc.pellucida.pager :as p]))
 
 (deftest paging
   (testing "normal usage"
-    (are [f t out] (= (paging-segment f t {:neighborhood 2 :min-elide 2}) out)
+    (are [f t out] (= (p/paging-segment f t {:neighborhood 2 :min-elide 2})
+                      out)
          10 0 []
          3 3 []
          3 4 []
@@ -15,24 +16,29 @@
          0 6 [1 2 ,3, 4 5]
          0 7 [1 2 nil 5 6]
          0 10 [1 2 nil 8 9])
-    (are [f c l out] (= (anchorhood f c l {:neighborhood 2 :min-elide 2}) out)
+    (are [f c l out] (= (p/anchorhood f c l {:neighborhood 2 :min-elide 2})
+                        out)
          0 0 0 [0]
          0 5 5 [0 1 2 3 4 5]
          0 6 6 [0 1 2 ,3, 4 5 6]
          0 0 7 [0 1 2 nil 5 6 7]
          0 7 10 [0 1 2 nil 5 6 7 8 9 10]))
   (testing "min neighborhood"
-    (are [f t out] (= (paging-segment f t {:neighborhood 0 :min-elide 2}) out)
+    (are [f t out] (= (p/paging-segment f t {:neighborhood 0 :min-elide 2})
+                      out)
          0 1 []
          0 2 [1]
          0 3 [nil])
-    (are [f c l out] (= (anchorhood f c l {:neighborhood 0 :min-elide 2}) out)
+    (are [f c l out] (= (p/anchorhood f c l {:neighborhood 0 :min-elide 2})
+                        out)
          0 2 4 [0 ,1, 2 ,3, 4]
          0 3 4 [0 nil 3 4]))
   (testing "min min-elide"
-    (are [f t out] (= (paging-segment f t {:neighborhood 2 :min-elide 1}) out)
+    (are [f t out] (= (p/paging-segment f t {:neighborhood 2 :min-elide 1})
+                      out)
          0 1 []
          0 5 [1 2 3 4]
          0 6 [1 2 nil 4 5])
-    (are [f c l out] (= (anchorhood f c l {:neighborhood 2 :min-elide 1}) out)
+    (are [f c l out] (= (p/anchorhood f c l {:neighborhood 2 :min-elide 1})
+                        out)
          0 6 12 [0 1 2 nil 4 5 6 7 8 nil 10 11 12])))
--- test/org/timmc/pellucida/util_test.clj
+++ test/org/timmc/pellucida/util_test.clj
@@ -1,9 +1,9 @@
 (ns org.timmc.pellucida.util-test
-  (:use clojure.test
-        org.timmc.pellucida.util))
+  (:use clojure.test)
+  (:require [org.timmc.pellucida.util :as u]))
 
 (deftest uri-codecs
-  (is (= (enc-pathc  "/?#% +") "%2F%3F%23%25%20%2B"))
-  (is (= (enc-queryc "/?#% +") "%2F%3F%23%25%20%2B"))
-  (is (= (dec-pathc  "%2F%3F%23%25%20%2B+") "/?#% ++"))
-  (is (= (dec-queryc "%2F%3F%23%25%20%2B+") "/?#% + ")))
+  (is (= (u/enc-pathc  "/?#% +") "%2F%3F%23%25%20%2B"))
+  (is (= (u/enc-queryc "/?#% +") "%2F%3F%23%25%20%2B"))
+  (is (= (u/dec-pathc  "%2F%3F%23%25%20%2B+") "/?#% ++"))
+  (is (= (u/dec-queryc "%2F%3F%23%25%20%2B+") "/?#% + ")))
