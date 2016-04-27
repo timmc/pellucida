a9192cdc6aa79e3402500f8f4f03f53a2b33cc72
Tim McCormack <cortex@brainonfire.net>
2016-04-26 22:35:08 -0400

    Move production NFSN config out

--- conf/prod-nfsn.clj
+++
@@ -1,5 +0,0 @@
-{:base-url "http://gallery-v2.brainonfire.net/"
- :thumbs-link-base "https://s3.amazonaws.com/cdn-pellucida.gallery.brainonfire.net/kpawebgen/images-v2/"
- :gallery-db "/home/protected/db/gallery.db3"
- :google-static-maps-v2-api-key-browser "AIzaSyALz6yR2Lw1liTI-97_5bwwGHBsZQH2SiM"
- :btc-donate-addr "15rmT5e3xDxGScJspJSZ9RemKHhxuBmejN"}
